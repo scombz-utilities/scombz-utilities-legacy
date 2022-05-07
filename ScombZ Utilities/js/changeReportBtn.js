2fc589a394c2153e380341bddda9459f5223089f
yudai1204 <59430508+yudai1204@users.noreply.github.com>
2022-05-08 00:52:39 +0900

    課題提出時間にスライダーバーを追加

--- ScombZ Utilities/js/changeReportBtn.js
+++ ScombZ Utilities/js/changeReportBtn.js
@@ -56,33 +56,52 @@ function createTimeTempBtn(){
     console.log('createTimeTempBtn');
     const $timeBoxes = document.getElementsByName("creationTime");
     for(const $timeBox of $timeBoxes){
-        $timeBox.parentNode.parentNode.parentNode.insertAdjacentHTML('beforeEnd',`
-        <style>
-        .minBtn{
-            margin:2px 0;
-            padding:2px 4px;
-            min-width:50px;
-            font-size:14px;
-        }
-        .minBtnArea{
-            width:100%;
-        }
-        </style>
-        <div class="minBtnArea">
-        <input type="button" value="10分" class="minBtn" style="margin-left:25px"/>
-        <input type="button" value="30分" class="minBtn"/>
-        <input type="button" value="60分" class="minBtn"/>
-        <input type="button" value="100分" class="minBtn"/>
-        <input type="button" value="200分" class="minBtn"/>
-        <input type="button" value="300分" class="minBtn"/>
-        </div>
-        `);
-        const $minBtns = document.getElementsByClassName("minBtn");
-        const mins=[10,30,60,100,200,300];
-        for(let i = 0; i < $minBtns.length; i++) {
-            $minBtns[i].addEventListener("click",function() {
-                $timeBox.value = mins[i%($minBtns.length/2)];
-            });
+        if($timeBox.type != "hidden"){
+            $timeBox.parentNode.parentNode.parentNode.insertAdjacentHTML('beforeEnd',`
+            <style>
+            .minBtn{
+                margin:2px 0;
+                padding:2px 4px;
+                min-width:50px;
+                font-size:14px;
+            }
+            .minBtnArea,minBarArea{
+                width:100%;
+            }
+            .min-slider-bar{
+                margin:10px 5%;
+                width:90%;
+            }
+            </style>
+            <div class="minBtnArea">
+            <input type="button" value="10分" class="minBtn" style="margin-left:25px"/>
+            <input type="button" value="30分" class="minBtn"/>
+            <input type="button" value="60分" class="minBtn"/>
+            <input type="button" value="100分" class="minBtn"/>
+            <input type="button" value="200分" class="minBtn"/>
+            <input type="button" value="300分" class="minBtn"/>
+            <input type="button" value="600分" class="minBtn"/>
+            </div>
+            <div class="minBarArea">
+            <input id="minSliderBar" type="range" min="0" max="600" step="30" class="min-slider-bar">
+            </div>
+            `);
+            if(!$timeBox.value){
+                $timeBox.value = 0;
+            }
+            const $minBtns = document.getElementsByClassName("minBtn");
+            const $minSliderBar = document.getElementById("minSliderBar");
+            const mins=[10,30,60,100,200,300,600];
+            for(let i = 0; i < $minBtns.length; i++) {
+                $minBtns[i].addEventListener("click",function() {
+                    $minSliderBar.value = $timeBox.value = mins[i%($minBtns.length/2)];
+                });
+            }
+            $minSliderBar.value = $timeBox.value;
+            console.log("MINSLIDERBAR: "+$minSliderBar.value);
+            $minSliderBar.addEventListener('input',(e)=>{
+                $timeBox.value = $minSliderBar.value;
+            },false);
         }
     }
     return;
