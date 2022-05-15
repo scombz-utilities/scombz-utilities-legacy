3676e6e0e8e3b0ff4ff5382c13898b735a4274c4
yudai1204 <59430508+yudai1204@users.noreply.github.com>
2022-05-15 18:12:17 +0900

    Add files via upload

--- ScombZ Utilities/js/styleSidemenu.js
+++ ScombZ Utilities/js/styleSidemenu.js
@@ -126,59 +126,54 @@ function styleSidemenu(){
         .sidemenu-link.sidemenu-lms-link.sidemenu-link-txt:hover{
             background:#f0f0f0;
         }
+        #sidemenu{
+            box-shadow:none;
+            overflow-y:auto;
+            position:fixed;
+            top:0;
+            left:0;
+            float:left;
+            z-index:100;
+        }
+        #pageMain{
+            position:absolute;
+            top:0;
+            left:0;
+            width:100%;
+            minW-width:371px;
+        }
+        .sidemenu-head{
+            height:60px;
+        }
+        .sidemenu-logo{
+            height:60px;
+            width:115px;
+            padding-top:0;
+            margin:0 auto;
+        }
+        #sidemenuClose{
+            left:0;
+            top:0;
+        }
+        .sidemenu-link{
+            height:50px;
+        }
+        .sidemenu-pull{
+            height:53px;
+            padding:17px 40px 12px 81px;
+        }
     </style>
     `);
     //ヘッダ中心にアイコンを表示 ヘッダをクリックで一番上へ
     const $pageHead = document.getElementById('page_head');
     $pageHead.insertAdjacentHTML('afterBegin',`<a href="javascript:void(0);" onclick='javascript:window.scrollTo({ top: 0, behavior: "smooth" });' class="pagetop-head"></a>`);
     $pageHead.insertAdjacentHTML('beforeEnd',`
-    <a href="/portal/home"><div class="mainmenu-head-logo"><img src="/sitelogo" class="scombz-icon" alt="Top"></div></a>
+    <a href="/portal/home" id="pagetop-head-logo"><div class="mainmenu-head-logo"><img src="/sitelogo" class="scombz-icon" alt="Top"></div></a>
     `);
-    //サイドメニューおよびメインカラムの設定
-    const $sidemenu = document.getElementById('sidemenu');
-        $sidemenu.style.boxShadow = ('none');
-        $sidemenu.style.overflowY = ('auto');
-        $sidemenu.style.position = ('fixed');
-        $sidemenu.style.top = ('0');
-        $sidemenu.style.left = ('0');
-        $sidemenu.style.float = ('left');
-        $sidemenu.style.zIndex = ('100');
-    const $pageMain = document.getElementById('pageMain');
-        $pageMain.style.position = ('absolute');
-        $pageMain.style.top = ('0');
-        $pageMain.style.left = ('0');
-        $pageMain.style.width = '100%';
-        $pageMain.style.minWidth = ('371px');
-    
-    //サイドメニューのヘッドサイズを小さくする
-    const $sidemenuHead = document.querySelector('.sidemenu-head');
-        $sidemenuHead.style.height = '60px';
-    //サイドメニューのロゴ小さく
-    const $sidemenuLogo = document.querySelector('.sidemenu-logo');
-        $sidemenuLogo.style.height = '60px';
-        $sidemenuLogo.style.width = '115px';
-        $sidemenuLogo.style.paddingTop = '0px';
-        $sidemenuLogo.style.margin = '0 auto';
     //サイドメニューの開閉ボタンを変える
     var $closeButton = document.getElementById('sidemenuClose');
         $closeButton.classList.add('hamburger-icon');
         $closeButton.innerHTML = '<div class="hamburger-line"></div>\n<div class="hamburger-line"></div>\n<div class="hamburger-line"></div>';
-        $closeButton.style.left = '0';
-        $closeButton.style.top = '0';
-    //各メニューの縦幅をすべて50pxにする
-    const $sidemenuLinkList = document.querySelectorAll('.sidemenu-link');
-    if($sidemenuLinkList[0]){
-        for (const $sidemenuLink of $sidemenuLinkList){
-            $sidemenuLink.style.height = '50px';
-        }
-    }
-    const $sidemenuPullList = document.querySelectorAll('.sidemenu-pull');
-    if($sidemenuPullList[0]){
-        for (const $sidemenuPull of $sidemenuPullList){
-            $sidemenuPull.style.height = '53px';
-            $sidemenuPull.style.padding = '17px 40px 12px 81px';
-        }
-    }
     //お知らせ、アンケートを直リンクにする
     //お知らせ(ついでにborder-topもつけてスタイル直す)
     const $infoButton = document.querySelector(".sidemenu-link.sidemenu-lms-link.sidemenu-link-txt.info-color.sidemenu-icon.info-icon");
