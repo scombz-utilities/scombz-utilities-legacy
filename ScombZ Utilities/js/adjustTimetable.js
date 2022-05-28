3501f0705c63eb885324e3af003316fb8e7332a4
Kyo-s-s <igagrid407@gmail.com>
2022-05-28 23:48:10 +0900

    LMSでのみ必要な処理をLMSにいるときにのみ動かすように

--- ScombZ Utilities/js/adjustTimetable.js
+++ ScombZ Utilities/js/adjustTimetable.js
@@ -6,44 +6,52 @@ function adjustTimetable(data, addSubTimetable){
     'use strict';
     let addcss = '<style type="text/css">';
 
+    if(location.href === 'https://scombz.shibaura-it.ac.jp/lms/timetable'){
+        // 土曜日削除
+        if(data.eraseSat === true){
+            const sats = Array.from(document.getElementsByClassName('6-yobicol'));
+            for(const sat of sats) sat.remove();
+            
+        }
+
+        // 5, 6限削除
+        const TableDataRow = Array.from(document.getElementsByClassName('div-table-data-row'));
+        if(TableDataRow.length > 0 && data.erase6) TableDataRow[5].remove();           
+        if(TableDataRow.length > 0 && data.erase7) TableDataRow[6].remove();
+        
+        // 教室名表示
+        if(data.dispClassroom === true){
+            const TableCellDetail = Array.from(document.getElementsByClassName('div-table-cell-detail'));
+            for(const cell of TableCellDetail){
+                let classroom = cell.firstElementChild.title;  
+                cell.firstElementChild.innerHTML = '<p style = "white-space: nowrap; overflow : hidden; text-overflow: ellipsis;">' + classroom + '</p>';
+            }
+        }
+
+        // センタリング
+        if(data.timetableCentering === true){
+            addcss += `
+                .div-table{
+                    text-align: center;
+                }
+            `;
+        }
+    }
+
     if(data.eraseSat === true){
-        const sats = Array.from(document.getElementsByClassName('6-yobicol'));
-        for(const sat of sats) sat.remove();
         addcss += `
             th.SubTimetable:nth-child(7), td.SubTimetable:nth-child(7) {
                 display: none;
             }
         `;
-    }
-    
-    if(data.timetableCentering === true){
-        addcss += `
-            .div-table{
-                text-align: center;
-            }
-        `;
-    }
-    
-    // TODO CSSで丸めをする
-    // 教室名表示　20文字を超えたら丸める
-    if(data.dispClassroom === true){
-        const TableCellDetail = Array.from(document.getElementsByClassName('div-table-cell-detail'));
-        for(const cell of TableCellDetail){
-            let classroom = cell.firstElementChild.title;  
-            if(classroom.length > 20) classroom = classroom.substring(0, 20) + '...';
-            cell.firstElementChild.innerHTML = '<p>' + classroom + '</p>';
-        }
-    }
-
-    const TableDataRow = Array.from(document.getElementsByClassName('div-table-data-row'));
-    if(TableDataRow.length > 0 && data.erase6) TableDataRow[5].remove();           
-    if(TableDataRow.length > 0 && data.erase7) TableDataRow[6].remove();
-
+    } 
     
     addcss += '</style>';
     document.head.insertAdjacentHTML('beforeEnd', addcss);
+    
 
 
+    // subTimetable の 5, 6限削除
     if(addSubTimetable === true){
         const existingTimetable = setInterval(function(){
             if(document.getElementById('subTimetable')){
