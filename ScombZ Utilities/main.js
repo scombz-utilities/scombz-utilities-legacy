94c5a9e57c6e3bb46fbb05354c6b51758155f9ad
yudai1204 <59430508+yudai1204@users.noreply.github.com>
2022-05-30 18:01:53 +0900

    ヘッダ中央ボタンのリンクを自由に選択できるように

--- ScombZ Utilities/main.js
+++ ScombZ Utilities/main.js
@@ -3,7 +3,7 @@
 (function(){
     'use strict';
     /*  定数  */
-    const $$version = '3.8.1';          //バージョン
+    const $$version = '3.8.2';          //バージョン
     const $$reacquisitionMin = 20;      //再取得までの時間(分)
     /*  定数ここまで  */
     console.log(`Welcome to ScombZ Utilities ver.${$$version}`);
