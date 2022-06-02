2eed31f6d8cef5411492ffafea9e7a74a5611e02
yudai1204 <59430508+yudai1204@users.noreply.github.com>
2022-06-02 16:42:43 +0900

    docs Update (#76)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

--- docs/js/mode.js
+++ docs/js/mode.js
@@ -1,12 +1,20 @@
-(function() {
+    console.log("loaded");
     const additionalModeArea = document.getElementById("additionalModeArea");
     if(additionalModeArea){
         let message = "";
         if(location.href.includes("mode=install")){
             message = `
+            <div>
             <h1>インストールありがとうございます！</h1>
             <p>このたびはScombZ Utilitiesのインストールをしただきありがとうございます!<br>個々の設定管理や、バグ報告などがございましたら<span class="extentionSettings">拡張機能設定</span>から設定してください！</p>
-            <p>ScombZ Utilitiesは、Chrome・FireFox・iOS/iPadOSに対応しています!気に入っていただけたら、下記リンクから導入していただくと嬉しいです</p>
+            <div>ScombZ Utilitiesをほかのブラウザでも使いませんか？
+                <ul>
+                    <li><a href="https://chrome.google.com/webstore/detail/scombz-utilities/iejnanaabfgocfjbnmhkfheghbkanibj?hl=ja" target="_blank" rel="noopener noreferrer">Chrome / Edge</a></li>
+                    <li><a href="https://addons.mozilla.org/ja/firefox/addon/scombz-utilities/"  target="_blank" rel="noopener noreferrer">FireFox</a></li>
+                    <!--li>iOS / iPadOS</li-->
+                </ul>
+            </div>
+            </div>
             `;
         }
         else if(location.href.includes("mode=install")){
@@ -16,4 +24,3 @@
         }
         additionalModeArea.insertAdjacentHTML("afterbegin",message);
     }
-})
