fca015dbc50ae389c50fd3695d0f9b86da1b50ae
yudai1204 <59430508+yudai1204@users.noreply.github.com>
2022-06-05 01:08:23 +0900

    docsUpdate

--- docs/js/mode.js
+++ docs/js/mode.js
@@ -20,48 +20,32 @@
         else if(location.href.includes("mode=update")){
             if(location.href.includes("version=")){
                 const version = location.href.slice(location.href.indexOf("version=")+8);
-                switch(version){
-                    case "3.9.2":
-                        message = `
-                        <div>
-                        <h1>アップデートされました</h1>
-                        <p>---アップデート情報 ~v3.9.2---<br>
-                        ・ScombZ由来のバグを修正 ドラッグ&ドロップで提出するモードになっていて、課題削除をしようとした場合に何も表示されなくなる不具合を修正しました<br>
-                        ・メニュー横の課題一覧に表示されるアンケート一覧を、課題一覧に沿ってソートするようにしました<br>
-                        ・ダークモードでのスタイルを修正しました<br>
-                        ・課題一覧で残り時間がずれる問題を修正しました<br>
-                        ・ブラウザ本体のアップデートでもランディングページが開く問題を修正しました<br>
-                        ・任意の課題を非表示にできる機能を追加しました<br>
-                        ・通知一括削除ボタンを追加しました<br>
-                        ・設定初期化ボタンを追加しました<br>
-                        ・崩れているレイアウトを修正しました<br>
-                        ・アップデート、インストール時に表示されるランディングページを追加しました<br>
-                        ・公式サイトへのリンクを追加しました<br>
-                        ・誤字を修正しました<br>
-                        ・設定のスタイルを変更しました</p>
-                        </div>`;
-                        break;
-                    case "3.9.1":
-                        message = `
-                        <div>
-                        <h1>アップデートされました</h1>
-                        <p>---アップデート情報 ~v3.9.1---<br>
-                        ・ダークモードでのスタイルを修正しました<br>
-                        ・課題一覧で残り時間がずれる問題を修正しました<br>
-                        ・ブラウザ本体のアップデートでもランディングページが開く問題を修正しました<br>
-                        ・任意の課題を非表示にできる機能を追加しました<br>
-                        ・通知一括削除ボタンを追加しました<br>
-                        ・設定初期化ボタンを追加しました<br>
-                        ・崩れているレイアウトを修正しました<br>
-                        ・アップデート、インストール時に表示されるランディングページを追加しました<br>
-                        ・公式サイトへのリンクを追加しました<br>
-                        ・誤字を修正しました<br>
-                        ・設定のスタイルを変更しました</p>
-                        </div>`;
-                        break;
-                    default:
-                        break;
-                }
+                $.getJSON("updateData.json", (jsondata) => {
+                    let mainHtmlData = "";
+                    for(const versionData of jsondata){
+                        if(versionData.version !== version){
+                            continue;
+                        }
+                        let subHtmlData = `
+                        <div class="version-column">
+                        <h2>ver.${versionData.version}</h2>
+                        <h3 class="browser-update-date">Chrome版アップデート:${versionData.date.chrome}</h3>
+                        <h3 class="browser-update-date">FireFox版アップデート:${versionData.date.firefox}</h3>
+                        `;
+                        for(let i=0;versionData.contents[i];i++){
+                            const contents = versionData.contents[i];
+                            subHtmlData += `
+                            <div class="update-details">
+                                <div class="update-type">${contents.genre}</div>
+                                <h4>${contents.title}</h4>
+                                <p>${contents.explain}</p>
+                            </div>`;
+                        }
+                        subHtmlData += 'より詳細なアップデート情報は<a href="updates.html">こちら</a></div>';
+                        mainHtmlData += subHtmlData;
+                    }
+                    message = mainHtmlData;
+                });
             }else{
                 message = `
                 <div>
@@ -75,6 +59,7 @@
                 ・公式サイトへのリンクを追加しました<br>
                 ・誤字を修正しました<br>
                 ・設定のスタイルを変更しました</p>
+                より詳細なアップデート情報は<a href="updates.html">こちら</a>
                 </div>
                 `;
             }
--- docs/js/update.js
+++ docs/js/update.js
@@ -6,13 +6,13 @@ if(updateArea){
             let subHtmlData = `
             <div class="version-column">
             <h2>ver.${versionData.version}</h2>
-            <h3>Chrome版アップデート:${versionData.date.chrome}</h3>
-            <h3>FireFox版アップデート:${versionData.date.firefox}</h3>
+            <h3 class="browser-update-date">Chrome版アップデート:${versionData.date.chrome}</h3>
+            <h3 class="browser-update-date">FireFox版アップデート:${versionData.date.firefox}</h3>
             `;
             for(let i=0;versionData.contents[i];i++){
                 const contents = versionData.contents[i];
                 subHtmlData += `
-                <div class = "update-details">
+                <div class="update-details">
                     <div class="update-type">${contents.genre}</div>
                     <h4>${contents.title}</h4>
                     <p>${contents.explain}</p>
