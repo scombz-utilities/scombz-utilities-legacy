f673aa8ee8f8153900c6a119aeff954279db1e34
yudai1204 <59430508+yudai1204@users.noreply.github.com>
2022-06-15 11:42:20 +0900

    Add files via upload

--- ScombZ Utilities/js/background.js
+++ ScombZ Utilities/js/background.js
@@ -1,14 +1,19 @@
 /* ScombZ Utilities */
 /* background.js */
-//オプションページを開く
-chrome.runtime.onMessage.addListener(function(message) {
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     switch (message.action) {
+        //オプションページを開く
         case "openOptionsPage":
             openOptionsPage();
+            break;
+        //jsonをgetで取得
+        case "getJson":
+            getJson(message,sender,sendResponse);
             break;
         default:
             break;
     }
+    return true;
 });
 
 function openOptionsPage(){
@@ -20,3 +25,25 @@ chrome.runtime.onInstalled.addListener(({reason}) => {
         chrome.tabs.create({url: `https://yudai1204.github.io/ScombZ-Utilities/?mode=${reason}&version=${chrome.runtime.getManifest().version}`});
     }
 });
+//getJson
+function getJson(message,sender,sendResponse){
+    fetch(message.endpoint, {
+        'method': 'GET',
+        'cache': 'no-store'
+    })
+    .then(function (res) {
+        return res.json(); // フェッチしたデータを JSON 形式に変換
+    })
+    .then(function (jsonData) {
+        sendResponse(jsonData); // JSON へ変換されたデータを返す
+        console.log("get done.");
+    })
+    .catch(error => {
+        console.log('エラーが発生しました');
+        sendResponse({
+            "title":"connection failed",
+            "reason":error,
+            "error":true
+        });
+    });
+}
---
+++ ScombZ Utilities/js/getnews.js
@@ -0,0 +1,87 @@
+function getNews(){
+    if(!location.href.includes("https://scombz.shibaura-it.ac.jp/portal/home") && !location.href.includes("https://scombz.shibaura-it.ac.jp/lms/timetable") ){
+        return;
+    }
+    chrome.storage.local.get({
+        getNewsData:[0]
+    },function(items){
+        const getNewsData = items.getNewsData;
+        setTimeout(function(){
+        console.log("===NOW LOADING===");
+        chrome.runtime.sendMessage(
+            {
+                action: 'getJson',
+                endpoint: 'https://yudai1204.github.io/ScombZ-Utilities/news/news.json'
+            },
+            (response) => {
+                if(response.error){
+                    //エラー時
+                    console.log(response);
+                    return;
+                }else{
+                    console.log("Utilitiesからのお知らせ取得成功");
+                    console.log(response);
+                    if(getNewsData.includes(response.length-1)) return;
+                    document.body.insertAdjacentHTML("beforeEnd",`
+                    <style>
+                    .utilities-news-box{
+                        display:block;
+                        width:300px;
+                        height:120px;
+                        background-color:#fff;
+                        border:1px solid #000;
+                        position:fixed;
+                        top:30px;
+                        right:10px;
+                        z-index:11;
+                        opacity:0.9;
+                        text-align:center;
+                        border-radius:5px;
+                        color:#000;
+                        font-size: 90%;
+                    }
+                    .utilities-news-remove{
+                        float:right;
+                        background-color:#fcc;
+                        margin-right:2px;
+                        margin-top:2px;
+                        width:18px;
+                        height:14px;
+                        cursor: pointer;
+                        font-size: 10px;
+                    }
+                    .utilities-news-remove:hover{
+                        background-color:#daa;
+                    }
+                    .utilities-news-box h3{
+                        font-size:14px;
+                        margin-bottom:5px;
+                    }
+                    .utilities-news-box *{
+                        margin:5px;
+                        padding:0;
+                    }
+                    </style>
+                    <div class="utilities-news-box">
+                    <div class="utilities-news-remove">×</div>
+                    <div style="padding:3px 10px;">
+                    <h3>ScombZ Utilitiesからのお知らせ</h3>
+                    <h4>${response[response.length-1].title}</h4>
+                    <p>${response[response.length-1].content}</p>
+                    <a href="${response[response.length-1].link}">詳細へ</a>
+                    </div>
+                    </div>
+                    `);
+                    document.querySelector(".utilities-news-remove").addEventListener("click", function(){
+                        document.querySelector(".utilities-news-box").remove();
+                        getNewsData.push(response[response.length-1].id);
+                        chrome.storage.local.set({
+                            getNewsData: getNewsData
+                        });
+                    });
+                }
+            }
+        );
+        },1000);
+    });
+}
