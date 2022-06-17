1acb3fcee4c1f29982429c6f4965d9195e56d987
Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
2022-06-17 21:36:58 +0900

    Fix the potential bug in `adfs.js` (#98)

--- ScombZ Utilities/js/adfs.js
+++ ScombZ Utilities/js/adfs.js
@@ -4,12 +4,17 @@
 function adfsLoaded(){
     'use strict';
         if (document.domain == 'adfs.sic.shibaura-it.ac.jp'){
-            console.log("ADFSをスキップします");
             const $adfsButton = document.getElementById("continueButton");
-            if ($adfsButton) {
+            const $adfsPin = document.getElementById("pin");
+
+            // ADFSの2段階認証が無効な場合にだけスキップを実行する
+            if ($adfsPin ?? $adfsButton) {
+                console.log("ADFSをスキップします");
                 $adfsButton.click();
+                console.log("ADFSをスキップしました");
+            } else {
+                console.log("ADFSの2段階認証が有効なため、スキップしません");
             }
-            console.log("ADFSをスキップしました");
         }
     return;
 }
