e9ad530e222b8a33e6cbe739d6bf2d76b3d4848e
Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
2022-06-17 18:37:45 +0900

    Modify adfs.js

--- ScombZ Utilities/js/adfs.js
+++ ScombZ Utilities/js/adfs.js
@@ -7,11 +7,13 @@ function adfsLoaded(){
             const $adfsButton = document.getElementById("continueButton");
             const $adfsPin = document.getElementById("pin");
 
-            // ADFSの2段階認証が無効な場合（$adfsPinがnullで存在しない時）にスキップする
-            if (!$adfsPin && $adfsButton) {
-              console.log("ADFSをスキップします");
+            // ADFSの2段階認証が無効な場合にだけスキップを実行する
+            if ($adfsPin ?? $adfsButton) {
+                console.log("ADFSをスキップします");
                 $adfsButton.click();
                 console.log("ADFSをスキップしました");
+            } else {
+                console.log("ADFSの2段階認証が有効なため、スキップしません");
             }
         }
     return;
