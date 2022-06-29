5c7f167c85392bd4cae92f3fd59d5366aa19e8a8
Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
2022-06-29 15:39:14 +0900

    Enable to click cotinueButton automatically when pin entered

--- ScombZ Utilities/js/adfs.js
+++ ScombZ Utilities/js/adfs.js
@@ -10,6 +10,14 @@ function adfsLoaded(){
             const $requiresPin = $hasAdfsButton && $adfsPin !==null;
             if ($requiresPin) {
                 console.log("ADFSの2段階認証が有効なため、スキップしません");
+                console.log("ADFSのワンタイムパスコード入力時の挙動を変更します");
+                const $adfsPinTip = document.querySelector("#customAuthArea > p:nth-child(3)");
+                $adfsPinTip.textContent = "MoSICAアプリを起動し、表示されている6桁のワンタイムパスコードを入力してください。";
+                const $adfsPinPattern = new RegExp("^[0-9]{6}$");
+                $adfsPin.oninput = (event) =>
+                    $adfsPinPattern.test(event.target.value) && $adfsButton.click();
+                $adfsPin.focus();
+                console.log("ADFSのワンタイムパスコード入力時の挙動を変更しました");
             } else if ($hasAdfsButton) {
                 console.log("ADFSをスキップします");
                 $adfsButton.click();
