dfff872f53d8a81cde30d42ab26e232130232ab1
Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
2022-11-20 10:44:05 +0900

    modifyCoursePageTitle で科目名が正しく表示されない不具合を修正 (#128)
    * Fix way to get the courseName
    
    * Prettier

--- ScombZ Utilities/js/modifyCoursePageTitle.js
+++ ScombZ Utilities/js/modifyCoursePageTitle.js
@@ -6,8 +6,10 @@ function modifyCoursePageTitle() {
         '#courseTopForm > div.course-header > div:nth-child(1) > div.course-title-txt.course-view-header-txt'
     )
     if (isCourseTopPage && courseTitle) {
-        // 科目名
-        const courseName = courseTitle.textContent.split(' ').at(-1)
+        // 科目名を取得する。例えば、「学部 01CD456789 Course Name」のような文字列から科目名（この例では「Course Name」）を抽出する
+        const courseName = courseTitle.textContent
+            .split(/ [0-9A-Za-z]{10} /)
+            .at(-1)
 
         // 「科目名 - 科目トップ」のような表示にする
         document.title = `${courseName} - ${document.title}`
