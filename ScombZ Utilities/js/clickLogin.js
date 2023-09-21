3db7d51efcd150bbbb3b9226d56d163ea0440bfd
Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
2023-09-21 22:51:20 +0900

    Fix clickLogin.js

--- ScombZ Utilities/js/clickLogin.js
+++ ScombZ Utilities/js/clickLogin.js
@@ -1,12 +1,11 @@
 /* ScombZ Utilities */
 /* clickLogin.js */
-function clickLoginBtn(){
-    'use strict';
-    if (location.href == 'https://scombz.shibaura-it.ac.jp/login'){
-        window.onload = function(){
-            console.log("ログインボタンをクリックします");
-            document.querySelector('.login-btn:nth-child(1)').click();
-            console.log("ログインボタンをクリックしました");
+function clickLoginBtn() {
+    "use strict";
+    if (location.href.startsWith("https://scombz.shibaura-it.ac.jp/login")) {
+        window.onload = function () {
+            console.log("ログインページへリダイレクトします");
+            location.href = "https://scombz.shibaura-it.ac.jp/saml/login?idp=http://adfs.sic.shibaura-it.ac.jp/adfs/services/trust";
         };
     }
     return;
